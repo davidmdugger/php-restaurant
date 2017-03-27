92a5e6261260204371b3f6c215c0e2a2d46d83f8
davidmdugger <davidmdj86@gmail.com>
2017-03-26 22:04:37 -0500

    Added my php code

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include('includes/header.php'); ?>
+
+<div class="container">
+    <div class="row">
+        <div id="home-content" class="col-sm-8 col-sm-offset-2">
+            <h2>David's Philosopy of Fine Sandwiches</h2>
+            <p>Here at Dugger's Deli I have the ultimate solution to satisfy your hunger. That's right, I'm talking sandwiches. PB&J, Turkey and Swiss, you name it; it just better be simple...and a sandwhich!</p>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
